abfb6336ef632a4fddccafefaf4d4593bba0a45f
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-15 20:57:41 -0400

    Added separate headers file

---
+++ headers.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+struct cow { char *name ; int numberOfFriends ;} ;
+char *makeName() ;
+struct cow getRandomCow() ;
+void display(struct cow c) ;
+void changeName(struct cow c, char newName) ;
+void changeNumberOfFriends(struct cow c, int newQuantity) ;

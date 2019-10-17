c5cee0ecd2d7b5ec1450f42183f08bfd78620166
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-16 21:12:02 -0400

    Worked through modifying and finished testing here

--- BeginningFile.c
+++ BeginningFile.c
@@ -1,38 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
-#include "headers.h"
 
 struct cow {
-  char *name ;
+  char name[11] ;
   int numberOfFriends ;
 } ;
 
-char *makeName() {
-    srand(time(NULL)) ;
-    char *list[15] ;
-    list[0] = "Johnny" ;
-    list[1] = "Loula" ;
-    list[2] = "Jackie" ;
-    list[3] = "Kathy" ;
-    list[4] = "Luis" ;
-    list[5] = "Luke" ;
-    list[6] = "Francine" ;
-    list[7] = "Timothy" ;
-    list[8] = "Avocado" ;
-    list[9] = "Will" ;
-    list[10] = "Daisy" ;
-    list[11] = "Wilson" ;
-    list[12] = "Eddie" ;
-    list[13] = "Abigail" ;
-    list[14] = "Macy" ;
-    return list[rand() % 15] ;
-}
+char names[20][10] = { "Johnny", "Loula", "Jackie", "Kathy", "Luis", "Luke", "Francine",
+                      "Timothy", "Avocado", "Will", "Daisy", "Wilson", "Eddie", "Abigail",
+                      "Macy", "Katie", "Willow", "Sandy", "CoolPerson", "Evangelina" } ;
 
 struct cow getRandomCow() {
+  srand(time(NULL)) ;
+  char s[11] ;
+  strcpy(s, names[rand() % 20]) ;
   struct cow q ;
-  q.name = makeName() ;
-  int n = abs(rand()) ;
+  strcpy(q.name, s) ;
+  int n = rand() ;
   n = n % 200 ;
   q.numberOfFriends = n ;
   return q ;
@@ -41,25 +27,32 @@ struct cow getRandomCow() {
 // display method prints out info about the cow given
 void display(struct cow c) {
   printf("Here is a cow and its info:\nName: %s\nNumber of friends: %d\n", c.name, c.numberOfFriends) ;
-
 }
 
 // modifying functions
-void changeName(struct cow c, char newName) {
-  c.name = &newName ;
+void changeName(struct cow * c, char* newName) {
+  strcpy(c -> name, newName) ;
 }
-void changeNumberOfFriends(struct cow c, int newQuantity) {
-  c.numberOfFriends = newQuantity ;
+void changeNumberOfFriends(struct cow * c, int newQuantity) {
+  c -> numberOfFriends = newQuantity ;
 }
 
 int main() {
-  int x ;
-  for (x = 0 ; x < 8 ; x++) {
-    struct cow a = getRandomCow() ;
-    display(a) ;
-  }
+  struct cow a = getRandomCow() ;
+  printf("\n") ;
+  display(a) ;
+  printf("\nLet's change the cow's name to Yoda!\n") ;
+  struct cow * p = &a ;
+  changeName(p, "Yoda") ;
+  printf("------------------------------------------------\nHere is updated information about the cow!\n------------------------------------------------\n") ;
+  display(a) ;
+  printf("\nLet's change how many friends your cow has! How about 1000000000 friends?\n") ;
+  changeNumberOfFriends(p, 1000000000) ;
+  printf("------------------------------------------------\nHere is updated information about the cow!\n------------------------------------------------\n") ;
+  display(a) ;
+  printf("Wow, this cow is very social! Look at how many friends they have!") ;
   printf("********************************************************\n") ;
-  printf("Here is a cow. What do you think it's name is?\n") ;
+  printf("Here is a cow. What do you think its name is?\n") ;
   printf("                                       /;    ;\\ \n") ;
   printf("                                   __  \\\\____//\n") ;
   printf("                                  /{_\\_/   `'\\____\n") ;

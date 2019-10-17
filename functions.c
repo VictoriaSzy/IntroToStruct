c4804151b20a47061fbafaadcadb666c70090030
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-16 21:08:01 -0400

    Added correct functions

--- functions.c
+++ functions.c
@@ -1,30 +1,12 @@
 #include "headers.h"
 
-char *makeName() {
-    srand(time(NULL)) ;
-    char *list[15] ;
-    list[0] = "Johnny" ;
-    list[1] = "Loula" ;
-    list[2] = "Jackie" ;
-    list[3] = "Kathy" ;
-    list[4] = "Luis" ;
-    list[5] = "Luke" ;
-    list[6] = "Francine" ;
-    list[7] = "Timothy" ;
-    list[8] = "Avocado" ;
-    list[9] = "Will" ;
-    list[10] = "Daisy" ;
-    list[11] = "Wilson" ;
-    list[12] = "Eddie" ;
-    list[13] = "Abigail" ;
-    list[14] = "Macy" ;
-    return list[rand() % 15] ;
-}
-
 struct cow getRandomCow() {
+  srand(time(NULL)) ;
+  char s[11] ;
+  strcpy(s, names[rand() % 20]) ;
   struct cow q ;
-  q.name = makeName() ;
-  int n = abs(rand()) ;
+  strcpy(q.name, s) ;
+  int n = rand() ;
   n = n % 200 ;
   q.numberOfFriends = n ;
   return q ;
@@ -35,9 +17,9 @@ void display(struct cow c) {
 
 }
 
-void changeName(struct cow c, char newName) {
-  c.name = &newName ;
+void changeName(struct cow * c, char* newName) {
+  strcpy(c -> name, newName) ;
 }
-void changeNumberOfFriends(struct cow c, int newQuantity) {
-  c.numberOfFriends = newQuantity ;
+void changeNumberOfFriends(struct cow * c, int newQuantity) {
+  c -> numberOfFriends = newQuantity ;
 }

14490603670694a123ba30803eed19ea58c06910
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-16 21:27:34 -0400

    Added complete struct definition

--- headers.h
+++ headers.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 
-struct cow { char *name ; int numberOfFriends ;} ;
+struct cow { char name[11] ; int numberOfFriends ;} ;
 struct cow getRandomCow() ;
 void display(struct cow c) ;
 void changeName(struct cow * c, char* newName) ;
